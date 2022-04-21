83ec40e37688f2c5fef237624c3313e88392a4f7
liomar.rabelo <liomar.rabelo@compasso.com.br>
2022-04-21 16:38:39 -0300

    App.js Update

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
+import RestaurantItem from './src/components/RestaurantItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Restaurant item */}
+      <RestaurantItem />
+      <RestaurantItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
 });
