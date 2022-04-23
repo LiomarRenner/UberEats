027c8a70d0bff3ec68700bac432e67a8a0a5fa2d
liomar.rabelo <liomar.rabelo@compasso.com.br>
2022-04-23 03:02:54 -0300

    Create RestaurantDetailsScreen

---
+++ src/screens/RestaurantDetailsScreen/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+
+// import { Container } from './styles';
+
+const RestaurantDetailsScreen: React.FC = () => {
+  return(
+    <View>
+        <Text>RestaurantDetailsScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+});
+export default RestaurantDetailsScreen;
