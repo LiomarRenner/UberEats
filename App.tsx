a62c596cdcb0ed813dc0d5183e374d0b668af02e
liomar.rabelo <liomar.rabelo@compasso.com.br>
2022-04-23 01:47:48 -0300

    Add: Flatlist

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import RestaurantItem from './src/components/RestaurantItem';
+import restaurants from './assets/data/restaurants.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Restaurant item */}
-      <RestaurantItem />
-      <RestaurantItem />
+      <FlatList 
+        data={restaurants} 
+        renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+        showsVerticalScrollIndicator={false} 
+      />
       <StatusBar style="auto" />
     </View>
   );
