07b6f3925abac0451acfdcc2dd719daa3392cfbe
liomar.rabelo <liomar.rabelo@compasso.com.br>
2022-04-23 03:34:58 -0300

    Update ResrautantDetails

--- src/screens/RestaurantDetailsScreen/index.tsx
+++ src/screens/RestaurantDetailsScreen/index.tsx
@@ -8,15 +8,21 @@ const restaurant = restaurants[0];
 const RestaurantDetailsScreen: React.FC = () => {
   return(
     <View style={styles.container}>
-        <Image 
-            source={{uri: restaurant.image}} 
-            style={styles.image} 
-            resizeMode='cover'
-        />
-        <Text style={styles.restaurantTitle}>{restaurant.name}</Text>
-        <Text style={styles.restaurantSubtitle}>
-            $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
-        </Text>
+      <Image 
+        source={{uri: restaurant.image}} 
+        style={styles.image} 
+        resizeMode='cover'
+      />
+
+			<View style={styles.iconContainer}>
+			</View>
+			
+      <View style={styles.detailsContainer}>
+				<Text style={styles.restaurantTitle}>{restaurant.name}</Text>
+				<Text style={styles.restaurantSubtitle}>
+					$ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
+				</Text>
+      </View>
     </View>
   )
 }
@@ -29,12 +35,21 @@ const styles = StyleSheet.create({
         width: '100%',
         aspectRatio: 5 / 3,
     },
+		iconContainer: {
+
+		},
+    detailsContainer: {
+        margin: 10,
+    },
     restaurantTitle:{
         fontSize: 36,
         fontWeight: '600',
+        marginVertical: 10,
+				color: '#131318',
     },
     restaurantSubtitle:{
-
+        fontSize: 14,
+        color: '#888',
     }
 });
 export default RestaurantDetailsScreen;
