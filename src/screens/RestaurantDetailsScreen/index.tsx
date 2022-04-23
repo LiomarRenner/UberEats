4e6fa640534db9629125902e5f9b0a3d9fc84f49
liomar.rabelo <liomar.rabelo@compasso.com.br>
2022-04-23 03:20:55 -0300

    Update RestaurantDetails

--- src/screens/RestaurantDetailsScreen/index.tsx
+++ src/screens/RestaurantDetailsScreen/index.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
-
+import restaurants from '../../../assets/data/restaurants.json';
 // import { Container } from './styles';
+const restaurant = restaurants[0];
+("");
 
 const RestaurantDetailsScreen: React.FC = () => {
   return(
-    <View>
-        <Text>RestaurantDetailsScreen</Text>
+    <View style={styles.container}>
+        <Image 
+            source={{uri: restaurant.image}} 
+            style={styles.image} 
+            resizeMode='cover'
+        />
+        <Text style={styles.restaurantTitle}>{restaurant.name}</Text>
+        <Text style={styles.restaurantSubtitle}>
+            $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
+        </Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+    container:{
+        flex: 1,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 5 / 3,
+    },
+    restaurantTitle:{
+        fontSize: 36,
+        fontWeight: '600',
+    },
+    restaurantSubtitle:{
+
+    }
 });
 export default RestaurantDetailsScreen;
